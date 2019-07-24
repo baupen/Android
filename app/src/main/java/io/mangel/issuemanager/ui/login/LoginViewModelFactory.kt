ae54d8fc0ec52478e6f42c5f54371724ee4bd4e7
Florian Moser <products@famoser.ch>
2019-07-24 11:05:05 +0200

    generate project

---
+++ app/src/main/java/io/mangel/issuemanager/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package io.mangel.issuemanager.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import io.mangel.issuemanager.data.LoginDataSource
+import io.mangel.issuemanager.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
